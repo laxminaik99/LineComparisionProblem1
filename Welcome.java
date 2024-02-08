f64d558cf380282115f9014e8ac1e1ac12a96681
laxminaik99 <lbnaik99@gmail.com>
2024-02-08 10:47:53 +0530

    first commit

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+package com.bridgelabz5;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+		
+
+	}
+
+}
